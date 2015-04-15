340bfe071f62c9bd0a37ba34adb235b2468401c2
Daniel Eastland <daniel.eastland@gmail.com>
2015-04-14 23:08:40 -0500

    added ss

---
+++ src/main/java/com/sfb/objects/SuicideShuttle.java
@@ -0,0 +1,95 @@
+package com.sfb.objects;
+
+public class SuicideShuttle extends Shuttle implements Seeker {
+
+	@Override
+	public void setTarget(Unit target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Unit getTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setType(String type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isSelfGuiding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSelfGuiding(boolean selfGuiding) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getEndurance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setEndurance(int endurance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLaunchImpulse() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLaunchImpulse(int launchImpulse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getWarheadDamage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWarheadDamage(int warheadDamage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setController(Unit controllingUnit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Unit getController() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int impact() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
